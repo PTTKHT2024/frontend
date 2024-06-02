1fc49aa677b72ebc5f30c168dc788d2ce288f586
Quankzz <165643643+Quankzz@users.noreply.github.com>
2024-06-02 13:20:36 +0700

    faet
    maintain service page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/layout/home/Home';
+import MaintainService from './components/service/MaintainService/MaintainService';
 
 function App() {
   return (
@@ -8,6 +9,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route
+            path="/dich-vu/dich-vu-bao-duong"
+            element={<MaintainService />}
+          />
         </Routes>
       </BrowserRouter>
     </>
