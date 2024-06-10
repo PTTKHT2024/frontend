7a62525a5e4ce869e861a27e2765bc1f048b47a2
Your Name <you@example.com>
2024-06-10 21:07:47 +0700

    add

--- src/components/technology/TSS/main.jsx
+++ src/components/technology/TSS/main.jsx
@@ -149,7 +149,7 @@ const TSS = ({ title, src }) => {
       {videoIndex !== null && (
         <div
           ref={videoRef}
-          className="fixed top-0 left-0 right-0 bottom-0 z-10 bg-gray-900 bg-opacity-75 flex justify-center items-center"
+          className="fixed top-0 left-0 right-0 bottom-0 z-[50] bg-gray-900 bg-opacity-75 flex justify-center items-center"
           onClick={handleClose}
         >
           <div
